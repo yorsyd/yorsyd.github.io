20ff365018fb65363be58e1693cdc2fe2bf57c42
herrosisgd27 <khairunsyah8935@gmail.com>
2023-07-03 00:49:43 +0700

    update login, profil, hapus_data, dashboard,infex(dikit), profil(dikit)

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 if(isset($_SESSION["login"])){
-  header("Location: index.php");
-  exit;
+  if ($_SESSION["login"] && $_SESSION["role"] === "admin") {
+    header("Location: dashboard.php");
+    exit;
+  } else {
+    header("Location: index.php");
+    exit;
+  }
 }
 
 require 'connect.php';
@@ -11,7 +16,7 @@ if(isset($_POST["login"])){
   $username = $_POST["username"];
   $password = $_POST["password"];
 
- $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+  $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 
   //cek username
   if(mysqli_num_rows($result) === 1){
@@ -19,15 +24,18 @@ if(isset($_POST["login"])){
     $row = mysqli_fetch_assoc($result);
     if(password_verify($password,$row["password"])){
         //set session
-        $_SESSION["login"] = true ;
+        $_SESSION["login"] = true;
+        $row["role"] = $_SESSION["role"] ;
 
-        header("Location: index.php");
+        if ($username === "admin") {
+          header("Location: dashboard.php");
+        } else {
+          header("Location: index.php");
+        }
         exit;
     }
   }
 
   $error = true;
-
 }
-
 ?>
