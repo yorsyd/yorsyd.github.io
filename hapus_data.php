4326c785ce15eb73f9b038438a12e697776df8df
yorsyd <yogarifn@gmail.com>
2023-07-03 15:11:32 +0700

    db

--- hapus_data.php
+++ hapus_data.php
@@ -7,18 +7,18 @@ if (isset($_GET['id_post']) && ctype_digit($_GET['id_post'])) {
     $id_post = $_GET['id_post'];
 
     // Query untuk menghapus data berdasarkan id_post
-    $query = "DELETE FROM penjualanbarang WHERE id = '$id_post'";
+    $query = "DELETE FROM jual1 WHERE id = '$id_post'";
     if (mysqli_query($conn, $query)) {
         echo "Data berhasil dihapus";
 
 
-        $maxIdQuery = "SELECT MAX(id) as max_id FROM penjualanbarang";
+        $maxIdQuery = "SELECT MAX(id) as max_id FROM jual1";
         $result = mysqli_query($conn, $maxIdQuery);
         $row = mysqli_fetch_assoc($result);
         $maxId = $row['max_id'];
       
         // Mengatur ulang nilai awal AUTO_INCREMENT pada kolom ID
-        $alterQuery = "ALTER TABLE penjualanbarang AUTO_INCREMENT = " . ($maxId + 1);
+        $alterQuery = "ALTER TABLE jual1 AUTO_INCREMENT = " . ($maxId + 1);
         mysqli_query($conn, $alterQuery);
 
         header("Location: dashboard.php");
