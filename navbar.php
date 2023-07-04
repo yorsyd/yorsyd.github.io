d46cb0d72ebbd98e89729aaf77ba94db8f76433f
yorsyd <yogarifn@gmail.com>
2023-07-04 14:22:09 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <!-- <title>Document</title> -->
         <meta charset="utf-8">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
             integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+            <title></title>
 
         <link rel="stylesheet" href="css/style-catalog-cart.css">
 
