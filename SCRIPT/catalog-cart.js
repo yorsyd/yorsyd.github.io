0b7377c88fc22c156c6857792775f1f8aed01594
kyASse <akyasmuhamadzaidan59@gmail.com>
2023-07-06 22:14:03 +0700

    Ganti data barang catalog

--- SCRIPT/catalog-cart.js
+++ SCRIPT/catalog-cart.js
@@ -17,75 +17,99 @@ closeShoppingBtn.addEventListener('click', () => {
 let products = [
     {
         id: 1,
-        name: 'KAOS',
-        image: 'kaos.png',
-        price: 150000
+        name: 'Nike Air Max 2013 Lime Green',
+        image: 'Nike/nike1.png',
+        price: 594000
     },
     {
         id: 2,
-        name: 'KEMEJA',
-        image: 'kemeja.png',
-        price: 200000
+        name: 'Nike Air Force 1 Low Yellow',
+        image: 'Nike/nike2.png',
+        price: 1300000
     },
     {
         id: 3,
-        name: 'CELANA',
-        image: 'celana.png',
-        price: 180000
+        name: "Nike Mens's Lupinek Flyknit",
+        image: 'Nike/nike3.png',
+        price: 986000
     },
     {
         id: 4,
-        name: 'SEPATU',
-        image: 'sepatu.png',
-        price: 250000
+        name: "Nike Kevin Durant 9 Black White",
+        image: 'Nike/nike4.png',
+        price: 650000
     },
     {
         id: 5,
-        name: 'KAOS',
-        image: 'kaos.png',
-        price: 150000
+        name: "Converse Jhon Varatos Black Low",
+        image: 'Converse/converse1.png',
+        price: 849000
     },
     {
         id: 6,
-        name: 'KEMEJA',
-        image: 'kemeja.png',
-        price: 200000
+        name: "Converse CT All Star High Black",
+        image: 'Converse/converse2.png',
+        price: 1059000
+    },
+    {
+        id: 6,
+        name: "Converse CT All Star Low Pink",
+        image: 'Converse/converse3.png',
+        price: 1250000
+    },
+    {
+        id: 6,
+        name: "Converse CT All Star High White",
+        image: 'Converse/converse4.png',
+        price: 1199000
     },
     {
         id: 7,
-        name: 'CELANA',
-        image: 'celana.png',
-        price: 180000
+        name: "Vans SK8 Hi Pro Black White",
+        image: 'Vans/vans1.png',
+        price: 849000
     },
     {
         id: 8,
-        name: 'SEPATU',
-        image: 'sepatu.png',
-        price: 250000
+        name: "Vans Slip on Checkerboard",
+        image: 'Vans/vans2.png',
+        price: 450000
     },
     {
         id: 9,
-        name: 'KAOS',
-        image: 'kaos.png',
-        price: 150000
+        name: "Vans Yatch Club Red Slip on",
+        image: 'Vans/vans3.png',
+        price: 469000
     },
     {
         id: 10,
-        name: 'KEMEJA',
-        image: 'kemeja.png',
-        price: 200000
+        name: "Vans Old School Black White",
+        image: 'Vans/vans4.png',
+        price: 749000
     },
     {
         id: 11,
-        name: 'CELANA',
-        image: 'celana.png',
-        price: 180000
+        name: "New Balance ML574CPJ",
+        image: 'New Balance/nb4.png',
+        price: 1299000
     },
     {
         id: 12,
-        name: 'SEPATU',
-        image: 'sepatu.png',
-        price: 250000
+        name: "New Balance 997 Brown",
+        image: 'New Balance/nb5.png',
+        price: 2250000
+    },
+    {
+        id: 13,
+        name: "New Balance 574 Black",
+        image: 'New Balance/nb2.png',
+        price: 1100000
+    },
+    {
+        id: 14,
+        name: "New Balance 373 Black",
+        image: 'New Balance/nb3.png',
+        price: 1370000
     },
 ];
 
